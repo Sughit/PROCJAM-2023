cd190b468bc516a9749d85e2e267a25b5ca77c75
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2023-12-03 17:06:09 +0200

    Valeu

--- EAR-3-PROCJAM-game/Assets/Scripts/WorkStation/Produce.cs
+++ EAR-3-PROCJAM-game/Assets/Scripts/WorkStation/Produce.cs
@@ -9,35 +9,58 @@ public class Produce : MonoBehaviour
     public float timer;
     public bool canTake;
     public Animator anim;
+    public GameObject foc;
+    bool onFire;
+    int x;
 
     void OnMouseUp()
     {
         Debug.Log("Clicked on ");
-        MakeProduct();
+        if(!onFire)
+            MakeProduct();
+        else
+        {
+            onFire = false;
+            foc.SetActive(false);
+            anim.SetBool("luatFoc", false);
+            x = 0;
+        }
     }
 
     public void MakeProduct()
     {
-        if(!canTake)
-        {
-            //sfx si particule
-            anim.SetBool("laMunca", true);
-            Debug.Log("Starting to make");
-            StartCoroutine(MakeProductTimer());
-        }
-        else
-        {
-            Debug.Log("Item waiting to be taken");
-        }
+            if(!canTake)
+            {
+                //sfx si particule
+                anim.SetBool("laMunca", true);
+                Debug.Log("Starting to make");
+                StartCoroutine(MakeProductTimer());
+            }
+            else
+            {
+                Debug.Log("Item waiting to be taken");
+            }
     }
 
     IEnumerator MakeProductTimer()
     {
+        x = Random.Range(0,12);
         yield return new WaitForSeconds(timer);
-        productGO.SetActive(true);
-        canTake = true;
-        anim.SetBool("laMunca", false);
-        Debug.Log("Finished");
-        //sunet finalizat
+        if(x != 1)
+        {
+            productGO.SetActive(true);
+            canTake = true;
+            anim.SetBool("laMunca", false);
+            Debug.Log("Finished");
+            //sunet finalizat
+        }
+        else
+        {
+            onFire = true;
+            foc.SetActive(true);
+            anim.SetBool("luatFoc", true);
+            anim.SetBool("laMunca", false);
+        }
+        
     }
 }
